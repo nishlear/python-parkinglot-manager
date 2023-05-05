38249d0cd207e32dde6f5bff1469112de24d29b5
nishlear <nishlear@gmail.com>
2023-05-05 11:49:27 +0700

    Simplified database to: member, staff, ticket and account.

--- parking.sql
+++ parking.sql
@@ -1,29 +1,36 @@
 ﻿CREATE DATABASE parking;
 USE parking;
 
-CREATE TABLE NguoiGuiXe (
-	phone varchar(10),
-    name varchar(255)
-);
-CREATE TABLE Xe (
-	plate varchar(255) PRIMARY KEY,
-    vehicleType int,
-    phone varchar(10)
-);
-CREATE TABLE VeGuiXe (
-	ticketID int NOT NULL AUTO_INCREMENT,
-    workerID int NOT NULL,
-    cash int NOT NULL,
-    plate varchar(255),
-    PRIMARY KEY (ID)
-);
-CREATE TABLE NhanVien (
-	workerID int NOT NULL AUTO_INCREMENT,
+CREATE TABLE member(
+	memberID int NOT NULL AUTO_INCREMENT,
     name varchar(255),
-    phone varchar(10),
-    PRIMARY KEY (workerID)
+    plate varchar(255),
+    PRIMARY KEY (memberID)
 );
-SET SQL_SAFE_UPDATES = 0;
 
-ALTER TABLE NhanVien ADD UNIQUE (phone);
-ALTER TABLE VeGuiXe ADD UNIQUE (plate);
+CREATE TABLE staff(
+	staffID int NOT NULL AUTO_INCREMENT,
+    name varchar(255),
+    phone varchar(255),
+    PRIMARY KEY (staffID)
+);
+
+CREATE TABLE ticket(
+	ticketID int NOT NULL AUTO_INCREMENT,
+    staffID int NOT NULL,
+    memberID int,
+    cash int,
+    plate varchar(255),
+    vehicletype int,
+    time_in datetime,
+    time_out datetime,
+    PRIMARY KEY (ticketID),
+    FOREIGN KEY (staffID) REFERENCES staff(staffID),
+    FOREIGN KEY (memberID) REFERENCES member(memberID)
+);
+
+ALTER TABLE ticket ADD UNIQUE (plate);
+ALTER TABLE staff ADD UNIQUE (phone);
+
+
+
